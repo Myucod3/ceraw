7e15ab0d03f28de07da2c2c4ddea1e45decd4ed4
IoMyu <132834449+Myucod3@users.noreply.github.com>
2026-05-19 14:27:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBAri-4BOPRmjOQCjFu28HsX-12v12clqo",
-  authDomain: "canli-f9093.firebaseapp.com",
-  projectId: "canli-f9093",
-  storageBucket: "canli-f9093.firebasestorage.app",
-  messagingSenderId: "798051879179",
-  appId: "1:798051879179:web:315a08c7eab5d3c2e83a0e",
-  measurementId: "G-NVTHJVKEJW"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
