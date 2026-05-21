3e39860e9903bd481edadbb427a0e414010b9535
IoMyu <132834449+Myucod3@users.noreply.github.com>
2026-05-21 14:26:24 +0200

    Update posts-script.js

--- posts-script.js
+++ posts-script.js
@@ -1,3 +1,5 @@
+const API_URL = 'https://telecharger-diff-suite-antenna.trycloudflare.com';
+
 let lastCount = 0;
 
 document.getElementById('upl-form').addEventListener("submit", async e => {
@@ -10,42 +12,51 @@ document.getElementById('upl-form').addEventListener("submit", async e => {
 });
 
 async function savePosts(){
-    const blank = document.createElement('canvas');
-    blank.width = canvas.width;
-    blank.height = canvas.height;
-
-    if (canvas.toDataURL() === blank.toDataURL()) {
-        alert('Рисуй!!!!!!!');
-        return
-    }
+    try{
+        const blank = document.createElement('canvas');
+        blank.width = canvas.width;
+        blank.height = canvas.height;
     
-    const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
-
-    const formData = new FormData();
-    formData.append('image', blob, 'drawing.png');
-
-    await fetch('https://telecharger-diff-suite-antenna.trycloudflare.com/drawings', {
-        method: 'POST',
-        body: formData,
-    });
-    lastCount = 0;
+        if (canvas.toDataURL() === blank.toDataURL()) {
+            alert('Рисуй!!!!!!!');
+            return
+        }
+        
+        const blob = await new Promise(resolve => canvas.toBlob(resolve, 'image/png'));
+    
+        const formData = new FormData();
+        formData.append('image', blob, 'drawing.png');
+    
+        await fetch(`${API_URL}/drawings`, {
+            method: 'POST',
+            body: formData,
+        });
+        lastCount = 0;
+    }catch (err){
+        console.error(err);
+        alert('Ошибка загрузки')
+    }
 }
 
 async function loadPosts(){
-    const response = await fetch('https://telecharger-diff-suite-antenna.trycloudflare.com/drawings');
-    const drawings = await response.json();
-
-    if (drawings.length === lastCount) return;
-
-    lastCount = drawings.length;
-    document.getElementById('post-el').innerHTML = '<h3>Uploads</h3>';
+    try{
+        const response = await fetch(`${API_URL}/drawings`);
+        const drawings = await response.json();
     
-    drawings.forEach(drawing => {
-        const img = document.createElement('img');
-        img.src = drawing.url;
-        img.id = "canvas-post";
-        document.getElementById('post-el').appendChild(img);
-    });
+        if (drawings.length === lastCount) return;
+    
+        lastCount = drawings.length;
+        document.getElementById('post-el').innerHTML = '<h3>Uploads</h3>';
+        
+        drawings.forEach(drawing => {
+            const img = document.createElement('img');
+            img.src = drawing.url + '?t=' + Date.now();
+            img.className = "canvas-post";
+            document.getElementById('post-el').appendChild(img);
+        });
+    }catch (err) {
+        console.error(err)
+    }
 }
 
 window.onload = async function(e){
