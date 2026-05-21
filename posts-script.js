bb7aed3d9518ac9f18e0a3601e59c8a26fd2b42d
IoMyu <132834449+Myucod3@users.noreply.github.com>
2026-05-21 03:15:22 +0200

    Change API endpoint for posts in posts-script.js
    Updated the API endpoint for saving and loading posts.

--- posts-script.js
+++ posts-script.js
@@ -13,14 +13,14 @@ async function savePosts(){
     const formData = new FormData();
     formData.append('image', blob, 'drawing.png');
 
-    await fetch('https://ceraw-backend.onrender.com/drawings', {
+    await fetch('https://following-accounts-compression-reduce.trycloudflare.com/drawings', {
         method: 'POST',
         body: formData,
     });
 }
 
 async function loadPosts(){
-    const response = await fetch('https://ceraw-backend.onrender.com/drawings');
+    const response = await fetch('https://following-accounts-compression-reduce.trycloudflare.com/drawings');
     const drawings = await response.json();
 
     drawings.forEach(drawing => {
